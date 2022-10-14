deaca22e70f6f84acf95b3d4c8b3c9010ee66df3
Antonio Melo <47642858+tonnyhms@users.noreply.github.com>
2022-10-14 14:51:56 -0300

    testing actions on github

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -15,11 +15,5 @@ module.exports = {
   "features": {
     "storyStoreV7": true
   },
-  viteFinal: (config, { configType }) => {
-    if (configType === 'PRODUCTION') {
-      config.base = '/lab-design-system/'
-    }
 
-    return config
-  }
 }
